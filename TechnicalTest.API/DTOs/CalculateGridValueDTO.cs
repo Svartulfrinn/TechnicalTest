b1c7e77066c0069dd644a5f6dcb1b4379d3d8a88
Svartúlfr <92615047+Svartulfrinn@users.noreply.github.com>
2022-07-03 04:22:51 +0100

    Add files via upload

--- TechnicalTest.API/DTOs/CalculateGridValueDTO.cs
+++ TechnicalTest.API/DTOs/CalculateGridValueDTO.cs
@@ -4,15 +4,15 @@
     {
         public GridDTO Grid { get; set; }
 
-        public List<Vertex> Vertices { get; set; }
+        public int x1 { get; set; }
+        public int y1 { get; set; }
+
+        public int x2 { get; set; }
+        public int y2 { get; set; }
+
+        public int x3 { get; set; }
+        public int y3 { get; set; }
 
         public int ShapeType { get; set; }
-    }
-
-    public class Vertex
-    {
-        public int x { get; set; }
-
-        public int y { get; set; }
     }
 }
